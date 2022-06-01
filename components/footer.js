869920a376946f08556eb565a51b8e05f314c5b9
Stevens <sdemervil@gmail.com>
2022-06-01 11:13:01 +0200

    refacto nav + footer

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
-        <footer class="py-4 bg-light mt-auto">
-        <div class="container-fluid px-4">
-          <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2022</div>
+        <footer className="py-4 bg-light mt-auto">
+        <div className="container-fluid px-4">
+          <div className="d-flex align-items-center justify-content-between small">
+            <div className="text-muted">Copyright &copy; Your Website 2022</div>
             <div>
               <a href="#">Privacy Policy</a>
               &middot;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../public/assets/styles/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faCoffee, faBars, faUser } from '@fortawesome/free-solid-svg-icons'
+library.add(faCheckSquare, faCoffee, faBars, faUser)
+
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  )
 }
 
 export default MyApp
