a98ba39fbbb6a56faf94ebc4a36cc9b9dc1016b2
Stevens <sdemervil@gmail.com>
2022-06-01 16:26:39 +0200

    fix layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckSquare, faCoffee, faBars, faUser, faGaugeHigh, faTableColumns, faAngleDown, faBookOpen, faChartArea, faTable, faArrowLeft, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 library.add(faCheckSquare, faCoffee, faBars, faUser, faGaugeHigh, faTableColumns, faAngleDown, faBookOpen, faChartArea, faTable, faArrowLeft, faMagnifyingGlass)
 
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }) {
 
